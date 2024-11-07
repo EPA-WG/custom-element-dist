1722e804a773ee00bd0555ddf1fc02b78d30d124
Sasha Firsov <suns@firsov.net>
2024-11-06 23:25:03 -0800

    material components.html added dropdown and menu

--- public/demo/z.js
+++
@@ -1,9 +0,0 @@
-    import circle from "circle";
-    import circle2 from "lib-root/circle.js";
-    console.log(circle())
-        try{
-
-            console.log(import.meta.resolve('lib-root/a.js'))
-        }catch( err ){
-            console.error(err.message)
-        }
