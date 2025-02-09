67ba2f5321055d551a98b37ef69f9968a2ff1629
Sasha Firsov <suns@firsov.net>
2025-02-09 00:38:12 -0800

    external-template.test.stories.ts async load custom-element.js enforce xmlns on output fragment

--- src/stories/external-template.test.stories.ts
+++ src/stories/external-template.test.stories.ts
@@ -5,6 +5,7 @@ import {expect, getByTestId, within} from '@storybook/test';
 
 import '../custom-element/custom-element.js';
 import {Demo, SrcAttribute}          from './location-element.test.stories';
+import {frameCanvas} from "./frame.canvas";
 
 type TProps = { title: string; body:string};
 
@@ -188,32 +189,34 @@ export const SvgWithinHtmlFile:Story  =
 };
 
 export const MathMLWithinHtmlFile:Story  =
-{   args : {title: 'external HTML template - MathML by id', body:`
-        <custom-element src="/html-template.html#sophomores-dream">
-            <template><i>loading MathML from HTML file ...</i></template>
-        </custom-element>
+{   args : {title: '6. external HTML template - MathML by id', body:`
+                <iframe src="../demo/external-templates-sb-6.html" data-testid="fr"></iframe>
+
 `}
 ,   play: async ({canvasElement}) =>
     {
         const canvas = within(canvasElement);
         await canvas.findByText(MathMLWithinHtmlFile.args!.title as string);
-        const ml = await canvas.findByTestId('ml-test');
+        const frCanvas = await frameCanvas('fr',canvas);
+
+        const ml = await frCanvas.findByTestId('ml-test');
         expect(ml.firstElementChild.localName).toEqual('mrow');
     },
 };
 
 export const ByIdWithinXsltFile:Story  =
-{   args : {title: 'external XHTML template - xsl by id', body:`
-        <custom-element src="/html-template.xhtml#embedded-xsl">
-            <template>whole XSLT is  embedded into HTML body</template>
-        </custom-element>
+{   args : {title: '7. external XHTML template - xsl by id', body:`
+        <iframe src="../demo/external-templates-sb-7.html" data-testid="fr"></iframe>
+
 `}
 ,   play: async ({canvasElement}) =>
     {
         const canvas = within(canvasElement);
         await canvas.findByText(ByIdWithinXsltFile.args!.title as string);
-        const ml = await canvas.findByTestId('src');
-        expect(ml.textContent).to.include('/html-template.xhtml#embedded-xsl');
+        const frCanvas = await frameCanvas('fr',canvas);
+
+        await expect( await frCanvas.findByText('whole XSLT is embedded into HTML body') ).toBeInTheDocument();
+        await expect( await frCanvas.findByText('./html-template.xhtml#embedded-xsl') ).toBeInTheDocument();
     },
 };
 export const MissingIdWithinXsltFile:Story  =
